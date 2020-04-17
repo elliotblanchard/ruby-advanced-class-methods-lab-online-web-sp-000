feeafbc03df448a3923383c86263578c388d79ce
Elliot Blanchard <public@elliotblanchard.com>
2020-04-17 20:43:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,9 @@ class Song
 
   def self.new_from_filename(filename)
     song = self.new
-
+    filename_elements = name.split(" - ")
+    song.name = filename_elements[1].split(".mp3")
+    song.artist_name = filename_elements[0]
     song
   end
 
