d93b2aa344d23a4e8312d20e1497e41d6bf4053d
Elliot Blanchard <public@elliotblanchard.com>
2020-04-17 20:23:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,9 +15,15 @@ class Song
   def self.new_by_name(name)
     song = self.new
     song.name = name
-    self.all << song
     song
   end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end  
 
   def save
     self.class.all << self
